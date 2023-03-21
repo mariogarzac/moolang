5a61d20589e856a4ee6475add3bdfcc3d610e1a5
mariogarzac <mariogarzac99@gmail.com>
2023-03-21 13:01:19 -0600

    removed unused tokens and fixed int and float order.

--- Tareas/MyLex.py
+++ Tareas/MyLex.py
@@ -1,8 +1,8 @@
 import ply.lex as lex
 
 tokens = (
+   'PROGRAMA',
    'ID',
-   'NUMBER',
    'COLON',
    'SEMICOL',
    'COMMA',
@@ -15,12 +15,9 @@ tokens = (
    'TIMES',
    'DIVIDE',
    'EQUAL',
-   'GREATER',
-   'LESSTHAN',
    'GT',
    'LT',
    'NOT',
-   'EQUALITY',
    'CTE_INT',
    'CTE_FLOAT',
    'CTE_STRING',
@@ -28,20 +25,18 @@ tokens = (
    'PRINT',
    'IF',
    'ELSE',
-   'INT',
    'FLOAT',
-   'PROGRAMA',
-   'PERIOD',
+   'INT',
 )
 
 reserved = {
+   'programa' : 'PROGRAMA',
    'var' : 'VAR',
    'print' : 'PRINT',
    'if' : 'IF',
    'else' : 'ELSE',
-   'int' : 'INT',
    'float' : 'FLOAT',
-   'programa' : 'PROGRAMA',
+   'int' : 'INT',
         }
 
 t_LPAREN =  r'\('
@@ -53,17 +48,13 @@ t_PLUS =  r'\+'
 t_MINUS =  r'-'
 t_TIMES =  r'\*'
 t_DIVIDE =  r'/'
-t_GREATER = r'<'
-t_LESSTHAN = r'>'
-t_GT = r'<='
-t_LT = r'>='
+t_GT = r'<'
+t_LT = r'>'
 t_NOT = r'<>'
-t_EQUALITY = '=='
 t_COMMA =  r','
 t_COLON =  r':'
 t_SEMICOL =  r';'
-t_CTE_STRING = r'"[A-Za-z_\s]*"'
-t_PERIOD = r'.'
+t_CTE_STRING = r'"[\w\d\s!?_\.]*"'
 t_ignore  = ' \t'
 
 def t_ID(t):
@@ -71,15 +62,16 @@ def t_ID(t):
     t.type = reserved.get(t.value,'ID')    # Check for reserved words
     return t
 
+def t_CTE_FLOAT(t):
+    r'[0-9]+\.[0-9]+'
+    t.value = float(t.value)
+    return t
+
 def t_CTE_INT(t):
     r'\d+'
     t.value = int(t.value)    
     return t
 
-def t_CTE_FLOAT(t):
-    r'[0-9]+\.?[0-9]*'
-    t.value = float(t.value)
-    return t
 
 def t_newline(t):
     r'\n+'
@@ -89,13 +81,15 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+with open('programa.txt', 'r') as file:
+    data = file.read()
 
 lexer = lex.lex()
 
-# lexer.input(data)
-# while True:
-#     tok = lexer.token()
-#     if not tok:
-#         break
-#     print(tok)
-#
+lexer.input(data)
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print(tok)
+
