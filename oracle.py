74cdf7f0d157e8c477f99c66ea153e1fc00ffb2a
mariogarzac <mariogarzac99@gmail.com>
2023-04-22 11:45:07 -0600

    Initial commit

---
+++ oracle.py
@@ -0,0 +1,146 @@
+
+"""
+TODO: Revisar cubo con la maestra
+- asingaciones
+- char
+- bools
+"""
+
+CUBE = {
+        int : {
+            'int' : {
+                '+':    'int'
+                '-':    'int'
+                '*':    'int'
+                '/':    'int'
+                '=':    'int'
+                '-gt':  'bool'
+                '-ge':  'bool'
+                '-lt':  'bool'
+                '-le':  'bool'
+                '-eq':  'bool'
+                '-ne':  'bool'
+            }, 
+
+            'float ': {
+                '+':    'float'
+                '-':    'float'
+                '*':    'float'
+                '/':    'float'
+                '=':    'float'
+                '-gt':  'bool'
+                '-ge':  'bool'
+                '-lt':  'bool'
+                '-le':  'bool'
+                '-eq':  'bool'
+                '-ne':  'bool'
+                  },
+
+            'char ': {
+                '+':    'char'
+                '-':    'char'
+                '*':    'char'
+                '/':    'char'
+                '=':    'char'
+                '-gt':  'bool'
+                '-ge':  'bool'
+                '-lt':  'bool'
+                '-le':  'bool'
+                '-eq':  'bool'
+                '-ne':  'bool'
+            }
+        }, # int
+
+        float : {
+            'int' : {
+                '+':    'float'
+                '-':    'float'
+                '*':    'float'
+                '/':    'float'
+                '=':    'float'
+                '-gt':  'bool'
+                '-ge':  'bool'
+                '-lt':  'bool'
+                '-le':  'bool'
+                '-eq':  'bool'
+                '-ne':  'bool'
+            },
+
+            'float ': {
+                '+':    'float'
+                '-':    'float'
+                '*':    'float'
+                '/':    'float'
+                '=':    'float'
+                '-gt':  'bool'
+                '-ge':  'bool'
+                '-lt':  'bool'
+                '-le':  'bool'
+                '-eq':  'bool'
+                '-ne':  'bool'
+            },
+
+            'char ': {
+                '+':    'char'
+                '-':    'char'
+                '*':    'char'
+                '/':    'char'
+                '=':    'char'
+                '-gt':  'bool'
+                '-ge':  'bool'
+                '-lt':  'bool'
+                '-le':  'bool'
+                '-eq':  'bool'
+                '-ne':  'bool'
+            },
+
+       }, # float
+
+        'char' : {
+            'int' : {
+                '+':    'char'
+                '-':    'char'
+                '*':    'char'
+                '/':    'char'
+                '=':    'char'
+                '-gt':  'bool'
+                '-ge':  'bool'
+                '-lt':  'bool'
+                '-le':  'bool'
+                '-eq':  'bool'
+                '-ne':  'bool'
+            },
+
+            'float ': {
+                '+':    'char'
+                '-':    'char'
+                '*':    'char'
+                '/':    'char'
+                '=':    'char'
+                '-gt':  'bool'
+                '-ge':  'bool'
+                '-lt':  'bool'
+                '-le':  'bool'
+                '-eq':  'bool'
+                '-ne':  'bool'
+            },
+
+            'char ': {
+                '+':    'char'
+                '-':    'char'
+                '*':    'char'
+                '/':    'char'
+                '=':    'char'
+                '-gt':  'bool'
+                '-ge':  'bool'
+                '-lt':  'bool'
+                '-le':  'bool'
+                '-eq':  'bool'
+                '-ne':  'bool'
+            }
+
+       } # char
+
+} # end
+
+
