7db2560b0970d181a25ab4ac09db6b5f03f84020
mariogarzac <mariogarzac99@gmail.com>
2023-05-01 20:08:45 -0600

    Finished?

--- FunctionDirectory.py
+++ FunctionDirectory.py
@@ -11,23 +11,20 @@ TODO:
 class Variable:
     def __init__ (self):
         self.varAttributes = {}
-   
-    def getVar(self):
-        return self.varAttributes
 
     def addVar(self, varId, varType, varScope, varValue, varXDims = None, varYDims = None):
-        self.varAttributes[varId] = {}
-        self.varAttributes[varId]["varType"] = varType
-        self.varAttributes[varId]["varScope"] = varScope
-        self.varAttributes[varId]["varValue"] = varValue
-        self.varAttributes[varId]["varXDims"] = varXDims
-        self.varAttributes[varId]["varYDims"] = varYDims
+        self.varAttributes = {varId :{"varType" : varType, "varScope" : varScope, "varValue" : varValue,\
+                "varXDims" : varXDims , "varYDims" : varYDims}}
+
+        # #self.funcDirectory[self.funcCounter]["funcVars"]= {}
+        # self.varAttributes = {}
+        # self.varAttributes[varId] = {}
+        # self.varAttributes[varId]["varType"] = varType
+        # self.varAttributes[varId]["varScope"] = varScope
+        # self.varAttributes[varId]["varValue"] = varValue
+        # self.varAttributes[varId]["varXDims"] = varXDims
+        # self.varAttributes[varId]["varYDims"] = varYDims
         return self.varAttributes
-    
-    
-    def printVars(self):
-        pretty = json.dumps(self.varAttributes, indent=4)
-        print(pretty)
 
 class FunctionDirectory:
 
@@ -36,13 +33,7 @@ class FunctionDirectory:
         # since functions will be addressed by number instead of name,
         # we start at 0
         self.funcCounter = 0
-        self.funcDirectory = {self.funcCounter: 
-                               {
-                                "funcType" : 0,
-                                "funcParams": {None},
-                                "funcVars" : {None}
-                               }
-                              }
+        self.funcDirectory = {self.funcCounter: { "funcType" : 0, "funcParams": {}, "funcVars" : {} } }
 
     def addFunc(self, funcId, funcType, funcParams = None, funcVars = None):
         # funcParams is a dictionary
@@ -58,28 +49,10 @@ class FunctionDirectory:
         self.funcDirectory[self.funcCounter]["funcParams"].update(paramId, paramType)
 
     def addVariable(self, newVar):
-        self.funcDirectory[self.funcCounter]["funcVars"].update(newVar)
-        # self.funcDirectory[self.funcCounter]["funcVars"]= howdy[varId]
-        # self.funcDirectory[self.funcCounter]["funcVars"][varId]["varType"] =  howdy[varId]
-        # self.funcDirectory[self.funcCounter]["funcVars"][varId]["varScope"] = howdy[varId][varScope]
-        # self.funcDirectory[self.funcCounter]["funcVars"][varId]["varValue"] = howdy[varId][varValue]
-        # self.funcDirectory[self.funcCounter]["funcVars"][varId]["varXDims"] = howdy[varId][varXDims]
-        # self.funcDirectory[self.funcCounter]["funcVars"][varId]["varYDims"] = howdy[varId][varYDims]
+        varId = list(newVar.keys())[0]
+        self.funcDirectory[self.funcCounter]["funcVars"][varId] = {}
+        self.funcDirectory[self.funcCounter]["funcVars"][varId].update(newVar[varId])
         return self.funcDirectory
 
     def printFuncDir(self):
         p.pprint(self.funcDirectory)
-
-
-vars = Variable()
-funcs = FunctionDirectory()
-tmp = vars.addVar("hello",1,20,4, None, None)
-funcs.addVariable(tmp)
-funcs.printFuncDir()
-
-
-
-
-
-# funcs.addVariable("howdy",1,20,4, None, None)
-# funcs.addVariable("greetings",1,20,4, None, None)
