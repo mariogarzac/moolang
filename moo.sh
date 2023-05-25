099a9eb04c8e4d989386d3a30649f0291c9af987
mariogarzac <mariogarzac99@gmail.com>
2023-05-24 21:08:39 -0600

    Aux file to run program

--- moo.sh
+++ moo.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+clear
 # rm parser.out
 # rm parsetab.py
-python3 mooYacc.py
+
+if [ -z "$1" ] || [-z "$2"]; then
+    python3 mooYacc.py 
+else
+    python3 mooYacc.py | sed -n "/Tests\/$1\.moo/,/$2\.moo/p"
+fi
+
