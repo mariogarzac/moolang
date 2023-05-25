6fd54b385ec56f45664340617823608d44d1ed9e
mariogarzac <mariogarzac99@gmail.com>
2023-05-24 21:12:24 -0600

    Updated function directory contetns and added validations for functions

--- FunctionDirectory.py
+++ FunctionDirectory.py
@@ -1,5 +1,6 @@
 import pprint as p
 import json 
+from cube import CONV
 
 class Variable:
     def __init__ (self):
@@ -8,7 +9,6 @@ class Variable:
     def addVar(self, varId, varType, varScope, varValue, varXDims = 0 , varYDims = 0):
         self.varAttributes = {varId :{"vType" : varType, "vScope" : varScope, "vValue" : varValue,\
                 "vXDims" : varXDims , "vYDims" : varYDims}}
-
         return self.varAttributes
 
     def printVars(self):
@@ -24,57 +24,87 @@ class FunctionDirectory:
         # since functions will be addressed by number instead of name,
         # we start at 0
         self.funcCounter = 0
-        self.funcDirectory = {self.funcCounter: {"fName": 0, "fType" : 0, "fParams": {}, "fVars" : {} } }
+        self.funcDirectory = {self.funcCounter: {"fName": 0, "fType" : 0, "fDir" : 0,"fResources": {CONV['int']:0, CONV['float']:0, CONV['char']:0, CONV['file']:0, CONV['bool']:0}, "fPTypes": [], "fVars" : {} } }
 
     def addFunc(self, funcId, funcType):
+        # Search for the function to see if it has already been declared
         for i in range(0,self.funcCounter + 1):
             if funcId == self.funcDirectory[i]["fName"]:
                 if funcType == self.funcDirectory[i]["fType"]:
-                    print(f"ERROR: Function {funcId} with type {funcType} has already been declared")
+                    fType = (list(CONV.keys())[list(CONV.values()).index(funcType)]) 
+                    print(f"ERROR: Function {funcId} with type {fType} has already been declared")
                     exit()
 
         self.funcCounter += 1
         self.funcDirectory[self.funcCounter] = {}
         self.funcDirectory[self.funcCounter]["fName"] = funcId
         self.funcDirectory[self.funcCounter]["fType"] = funcType
+        self.funcDirectory[self.funcCounter]["fPTypes"] = []
         self.funcDirectory[self.funcCounter]["fParams"] = {}
         self.funcDirectory[self.funcCounter]["fVars"] = {}
 
     def addParam(self, paramId, paramType): 
-        self.funcDirectory[self.funcCounter]["fParams"].update({paramId : paramType})
+        self.funcDirectory[self.funcCounter]["fVars"].update({paramId : paramType})
+        self.funcDirectory[self.funcCounter]["fPTypes"].append(paramType)
 
     def addVariable(self, newVar):
         # get var id from newvar dictionary 
         varId = list(newVar.keys())[0]
+
+        # create a new dictionary with the varId and fill
         self.funcDirectory[self.funcCounter]["fVars"][varId] = {}
         self.funcDirectory[self.funcCounter]["fVars"][varId].update(newVar[varId])
         return self.funcDirectory
 
+    def updateFuncDir(self, pointer):
+        self.funcDirectory[self.funcCounter]["fDir"] = pointer - 1
+
     def assignValue(self,varId, value):
-        self.funcDirectory[self.funcCounter]["fVars"][str(varId)]["vValue"] = value
+        existsLocal = True
+        # print(varId)
+        # find in local scope
+        try:
+            self.funcDirectory[self.funcCounter]["fVars"][str(varId)]["vValue"] = value
+        except KeyError:
+            # existsLocal = False
+            pass
+
+        if (not existsLocal):
+            try:
+                self.funcDirectory[0]["fVars"][str(varId)]["vValue"] = value
+            except KeyError:
+                print(f"ERROR: Variable {varId} does not exist")
+                exit()
 
     def getVarType(self, varId):
         id = str(varId)
         # find in local scope
         try:
             return self.funcDirectory[self.funcCounter]["fVars"][id]["vType"]
-            # find in global scope
+        except KeyError:
+            existsLocal = False
+            pass
+
+        # find in global scope
+        if (not existsLocal):
             try:
                 return self.funcDirectory[0]["fVars"][id]["vType"]
             except KeyError:
-                pass
-        except KeyError:
-            print(f"ERROR: Variable {varId} does not exist")
-            exit()
+                print(f"ERROR: Variable {varId} does not exist")
+                exit()
+
+    def getFuncType(self):
+        return self.funcDirectory[self.funcCounter]["fType"]
+
+    def getFuncParams(self):
+        return self.funcDirectory[self.funcCounter]["fPTypes"]
 
     def printFuncDir(self):
         print(json.dumps(self.funcDirectory, indent=4, sort_keys=False))
 
     # DEBUGGING
     def clearFD(self):
+        self.printFuncDir()
         self.funcCounter = 0
         self.funcDirectory = {}
-        self.funcDirectory = {self.funcCounter: {"fName": 0, "fType" : 0, "fParams": {}, "fVars" : {} } }
-
-
-
+        self.funcDirectory = {self.funcCounter: {"fName": 0, "fType" : 0, "fDir" : 0,"fResources": {1:0, 2:0, 3:0, 4:0, 5:0}, "fPTypes": [], "fVars" : {} } }
