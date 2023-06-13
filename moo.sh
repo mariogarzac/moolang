bfe1147832b0c32d6e9e61eb4752179293e484b4
mariogarzac <mariogarzac99@gmail.com>
2023-06-13 11:17:45 -0600

    Modified script to install modules needed for the moolang compiler

--- moo.sh
+++ moo.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 clear
-# rm parser.out
-# rm parsetab.py
 
-python3 mooYacc.py 
+modules=("cryptography" "pickle" "ply.lex" "ply.yacc" "hashlib" "json")
+install_marker=".installed"
+
+if [ ! -f "$install_marker" ]; then
+    for module_name in "${modules[@]}"; do
+        if python3 -c "import $module_name" &> /dev/null; then
+            echo "$module_name is already installed."
+        else
+            echo "$module_name is not installed. Installing..."
+            pip install $module_name
+            echo "$module_name has been successfully installed."
+        fi
+    done
+
+    touch "$install_marker"
+fi
+
+python3 mooYacc.py
 python3 VirtualMachine.py
 
